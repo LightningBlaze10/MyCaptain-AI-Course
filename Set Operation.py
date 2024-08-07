a48312cc076ff4575e57f9e663998763f9cc2533
Tejesh <145763192+LightningBlaze10@users.noreply.github.com>
2024-08-07 17:56:42 +0400

    Set Operations
    Operations on Sets in python

---
+++ Set Operation.py
@@ -0,0 +1,7 @@
+E = {8, 0, 2, 6, 4}
+N = {2, 4, 5, 1, 3}
+
+print("Union of E & N is", E.union(N))
+print("Intersection of E & N is", E.intersection(N))
+print("Difference of E and N is", E.difference(N))
+print("Symmetric Difference of E and N is", E.symmetric_difference(N))
